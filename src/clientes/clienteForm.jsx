783f9d368031ef2d4a51183f3a5f00d81d8805ac
Maria Roberta <mariarobertaprado@outlook.com>
2022-07-25 21:29:10 -0300

    [Feature] adding beauty to my register form =]

--- src/clientes/clienteForm.jsx
+++ src/clientes/clienteForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import {reduxForm, Field} from "redux-form"
+import labelAndInput from "../common/form/labelAndInput"
 
 class ClienteForm extends Component {
     render (){
@@ -9,9 +10,12 @@ class ClienteForm extends Component {
             
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name="nome" component="input"/>
-                    <Field name="cpf" component="input"/>
-                    <Field name="telefone" component="input"/>
+                    <Field name="nome" component={labelAndInput}
+                    label="Nome"  cols="12 4" placeholder="Informe o nome"/>
+                    <Field name="cpf" component={labelAndInput}
+                    label="CPF"  cols="12 4" placeholder="Informe o CPF"/>
+                    <Field name="telefone" component={labelAndInput}
+                    label="Telefone"  cols="12 4" placeholder="Informe o Telefone"/>
 
                 </div>
                 <div className="box-footer">
