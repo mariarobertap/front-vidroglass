7518fef7719f2da053487ce913e03553c0d3d1d4
Maria Roberta <mariarobertaprado@outlook.com>
2022-10-01 21:37:40 -0300

    [Feat] - customer delete option

--- src/clientes/clienteList.jsx
+++ src/clientes/clienteList.jsx
@@ -1,52 +1,56 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import { getList, showUpdate, showDelete } from './clienteActions'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { getList, showUpdate, showDelete } from "./clienteActions";
 
-class BillingCycleList extends Component {
+class ClienteList extends Component {
+  componentWillMount() {
+    this.props.getList();
+  }
+  renderRow() {
+    const list = this.props.list || [];
 
-    componentWillMount() {
-        this.props.getList()
-    }
-    renderRow() {
-        const list = this.props.list || []
+    return list.map((bc) => (
+      <tr key={bc.idcliente}>
+        <td>{bc.nome}</td>
+        <td>{bc.telefone}</td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => this.props.showUpdate(bc)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.props.showDelete(bc)}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </td>
+      </tr>
+    ));
+  }
 
-        return list.map(bc => (
-            <tr key={bc.idcliente}>
-                <td>{bc.nome}</td>
-                <td>{bc.telefone}</td>
-                <td>
-                    <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)}>
-                        <i className='fa fa-pencil'></i>
-                    </button>
-                    <button className='btn btn-danger' onClick={() => this.props.showDelete(bc)}>
-                        <i className='fa fa-trash-o'></i>
-                    </button>
-                </td>
+  render() {
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Telefone</th>
+              <th className="table-actions">Ações</th>
             </tr>
-        ))
-    }
-
-    render() {
-        return (
-            <div>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Telefone</th>
-                            <th className='table-actions'>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {this.renderRow()}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+          </thead>
+          <tbody>{this.renderRow()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({list: state.cliente.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate, showDelete}, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
+const mapStateToProps = (state) => ({ list: state.cliente.list });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(ClienteList);
--- src/product/comboBox.jsx
+++ src/product/comboBox.jsx
@@ -1,39 +1,41 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import { getList } from '../productType/productTypeActions'
-import Grid from '../common/layout/grid'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { getList } from "../productType/productTypeActions";
+import Grid from "../common/layout/grid";
 
 class ComboBox extends Component {
+  componentWillMount() {
+    this.props.getList();
+  }
 
-     componentWillMount() {
-         this.props.getList()
-     }
+  renderRows() {
+    const list = this.props.list || [];
 
-    renderRows() {
-        const list = this.props.list || []
+    return list.map((bc) => <option value={bc.nome}>{bc.nome}</option>);
+  }
 
-        return list.map(bc => (
-            <option value={bc.nome}>{bc.nome}</option>
-        ))
-    }
+  render() {
+    return (
+      <Grid cols="12 4">
+        <div>
+          <label>Tipo do produto</label>
 
-    render() {
-        return (
-            <Grid cols="12 4">
-                <div>
-                        <label>Tipo do produto</label>
-
-                        <select className="form-control select2 select2-hidden-accessible" cols="12 4"  placeHolder= "teste" name="cars">
-                        {this.renderRows()}
-                        </select>
-                </div>
-              
-           </Grid>
-        )
-    }
+          <select
+            className="form-control select2 select2-hidden-accessible"
+            cols="12 4"
+            placeHolder="teste"
+            name="cars"
+          >
+            {this.renderRows()}
+          </select>
+        </div>
+      </Grid>
+    );
+  }
 }
 
-const mapStateToProps = state => ({list: state.tipo_produto.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(ComboBox)
+const mapStateToProps = (state) => ({ list: state.produto.list });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getList }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(ComboBox);
